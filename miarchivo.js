a83c4ad0fc0d87a829ba70fd6a5eaa8a1d933cac
prieto-pe <128441767+prieto-pe@users.noreply.github.com>
2024-05-17 04:37:42 -0300

    Update miarchivo.js

--- miarchivo.js
+++ miarchivo.js
@@ -65,119 +65,212 @@ cantidadAlumnos = listadoAlumnos.length;
 
 
 //// Pedir datos para ingresar un nuevo alumno y guardar en Array
+
 function registrarAlumno(){
 
+    let formularioRegistrarAlumno = document.getElementById("registrarAlumno-form");
+    formularioRegistrarAlumno.addEventListener("submit", completarFormulario);
     
-    alert("Por favor, complete los siguientes datos para registrar a un alumno");
-    
-    listadoAlumnos.push(new Alumno(
-        prompt("Nombre: "),
-        prompt("Apellido: "),
-        prompt("DNI: "),
-        prompt("Fecha de Nacimiento: ")
-    ));
+    function completarFormulario(e) {
+
+        e.preventDefault();
+        
+        let nombreAlumno = document.getElementById("nombreAlumno").value;
+        let apellidoAlumno = document.getElementById("apellidoAlumno").value;
+        let dniAlumno = document.getElementById("dniAlumno").value;
+        let fechaNacimientoAlumno = document.getElementById("fechaNacimientoAlumno").value;
+
+        listadoAlumnos.push(new Alumno(
+            nombreAlumno,
+            apellidoAlumno,
+            dniAlumno,
+            fechaNacimientoAlumno
+        ));
 
     alert("Se ha registrado a " + listadoAlumnos[listadoAlumnos.length - 1].nombreCompleto + " con exito.");
+    cantidadAlumnos = listadoAlumnos.length; // Actualiza cantidad de Alumnos
     
-    cantidadAlumnos = listadoAlumnos.length; // Actualizar cantidad de Alumnos
-    listadoAlumnos.nombreCompleto.sort();
 
-};
+    formularioRegistrarAlumno.removeEventListener("submit", completarFormulario);
 
-//// Poner nota a todos los alumnos de la clase (Materia, Valor de Evaluacion)
-function calificarAlumnos() {
-    
-    let clase  = prompt("Clase en la que se evaluara: ")
-    let evento = prompt("Evento a calificar: ")
-    
-    for (let i = 0; i <= cantidadAlumnos; i++) {
+    }
 
-        alert("Calificara al alumno " + listadoAlumnos[i].nombreCompleto + ".");
-        
-        listadoAlumnos[i].agregarCalificacion(
-            clase,
-            evento,
-            prompt("Calificacion del Evento (del 1 al 10): ") // Luego cambiar a listado con validacion de datos para evitar valores incorrectos
-        )
+    formularioRegistrarAlumno.addEventListener("submit", ocultarFormulario);
 
-        let calificacionAlumno = listadoAlumnos[i].calificacionesAlumno[length].calificacion
-    
-        if (calificacionAlumno >= 7 && calificacionAlumno <=10) {
-            document.write("El alumno " + listadoAlumnos[i].nombreCompleto + " ha aprobado con: " + calificacionAlumno + "</br>");
+    //Ocultar Formulario y cambiar color al boton
+    function ocultarFormulario(e) {
 
-        } else if (calificacionAlumno >= 1 && calificacionAlumno < 7){
-            document.write("El alumno " + listadoAlumnos[i].nombreCompleto + " ha desaprobado con: " + calificacionAlumno + "</br>");
+        e.preventDefault();
 
-        } else {
-            document.write("El alumno " + listadoAlumnos[i].nombreCompleto + " tuvo una nota que no es valida" + "</br>");
-        };
+        document.getElementById("registrarAlumno-form").reset();
+        document.getElementById("formularioRegistrarAlumno").style.display = "none";
+
+        button1.style.cssText =
+    "background-color: gray; color: white; border: none; padding: .5em; border-radius: 5px;";
     }
 
 };
 
-//// Tomar presente a los Alumnos 
-function tomarPresente(){
+//// Poner nota a todos los alumnos de la clase (Materia, Valor de Evaluacion)
 
-// Sumar una clase nueva
-cantidadClases.push(1)
+function calificarAlumnos() {
+    
+    const botonSeleccionarMateria = document.querySelector("#seleccionarEvento-btn");
 
-alert("A continuacion marcara los presentes por alumno.")
+    // Boton para seleccionar la Materia y el evento
+    botonSeleccionarMateria.onclick = () => {
 
-for (let i = 0; i < cantidadAlumnos; i++) {
+        //Seleccionar la Materia de la clase
+        let select = document.getElementById("materia-slct");
+        let materia = select.options[select.selectedIndex].text
 
-    let nombreCompletoAlumno = listadoAlumnos[i].nombreCompleto
+        //Nombre del evento de la materia
+        let evento = document.getElementById("nombreEvento").value;
 
-    let estado = prompt("Alumno: " + nombreCompletoAlumno + ".\n0 - Ausente. \n1 - Presente.")
+        //ocultar los inputs para poner la informacion de la materia y evento
+        document.getElementById("materia-slct").style.display = "none";
+        document.getElementById("nombreEvento").style.display = "none";
+        document.getElementById("materiaLabel").innerText = materia
+        document.getElementById("eventoLabel").innerText = evento
+        botonSeleccionarMateria.style.display = "none";
 
-    listadoAlumnos[i].agregarPresente(estado)
+        //poner informacion de la materia y evento en el sessionStorage para usarlo al finalizar la calificacion
+        sessionStorage.setItem("materia", materia);
+        sessionStorage.setItem("evento", evento);
+
+    }
+
+let calificacionesJSON = [];
+let ordenAlumno = 0;
+
+function mostrarAlumno() {
+    // Mostrar el nombre del alumno actual a calificar
+    let nombreAlumnoACalificar = listadoAlumnos[ordenAlumno].nombreCompleto;
+    document.getElementById("nombreAlumnoNota").innerText = "Alumno: " + nombreAlumnoACalificar + ".";
+}
+
+function calificarAlumno() {
+    // Obtener el valor de la calificación del alumno actual
+    let calificacionAlumno = document.getElementById("nota").value;
+    const nombreAlumnoACalificar = listadoAlumnos[ordenAlumno].nombreCompleto;
+    
+    const estado = calificacionAlumno == 0 ? "Ausente" :
+                   calificacionAlumno < 7 ? "Reprobado" : "Aprobado";
+
+        // Guardar los datos calificados
+        calificacionesJSON.push({
+            materia: sessionStorage.getItem("materia"),
+            evento: sessionStorage.getItem("evento"),
+            nombreAlumno: nombreAlumnoACalificar,
+            calificacion: calificacionAlumno,
+            estado: estado
+        });
+
+    // Limpiar el campo de la nota para el próximo alumno
+    document.getElementById("nota").value = "";
+
+    // Incrementar el índice del alumno
+    ordenAlumno++;
+
+    // Verificar si quedan alumnos por calificar
+    if (ordenAlumno < listadoAlumnos.length) {
+        mostrarAlumno();
+    } else {
+        //simulador con alert que se envio en formato JSON a una base de datos
+        alert(JSON.stringify(calificacionesJSON));
+        sessionStorage.removeItem("materia");
+        sessionStorage.removeItem("evento");
+
+        //Ocultar Formulario y cambiar color al boton
+        document.getElementById("calificarAlumnos-form").reset();
+        document.getElementById("calificarAlumnos-form").style.display = "none";
+
+        button2.style.cssText =
+        "background-color: gray; color: white; border: none; padding: .5em; border-radius: 5px;";
+
+        //avisar que finalizo el evento
+        document.getElementById("finEvento").innerText = "Todos los alumnos fueron calificados. Evento Finalizado."
+    }
+
+    }
+
+    const botonCalificarAlumno = document.querySelector("#calificarAlumno-btn");
+
+    // Inicializar mostrando el primer alumno
+    mostrarAlumno();
+
+    // Pasar al siguiente alumno al hacer clic en el botón
+    botonCalificarAlumno.onclick = calificarAlumno;
 
 }
 
-// Terminar calculando el porcentaje de ausentes de la clase y con un filter por alumnos ausentes
-let ausentes = listadoAlumnos.filter(ausente => ausente.presentesAlumno[length] == 0).map(ausente => "\n" + ausente.nombreCompleto)
-let porcentaAusentes = (ausentes.length / cantidadAlumnos) * 100;
+  //Funcion para seleccionar la nota del Alumno
+  addEventListener("load",inicio,false);
 
-alert("En la clase de hoy hubo " + porcentaAusentes + "% de ausentes.");
-alert("Los alumnos ausentes fueron:" + ausentes + ".");
+  function inicio() {
+      document.getElementById("nota").addEventListener("change",cambioDeNota,false);
+  }
 
-}
+  function cambioDeNota() {    
+      document.getElementById("notas").innerHTML=document.getElementById("nota").value;
+  }
+
+//// Tomar presente a los Alumnos //// cambiar para que sea compatible con el DOM
+// function tomarPresente(){
+
+// // Sumar una clase nueva
+// cantidadClases.push(1)
+
+// alert("A continuacion marcara los presentes por alumno.")
+
+// for (let i = 0; i < cantidadAlumnos; i++) {
+
+//     let nombreCompletoAlumno = listadoAlumnos[i].nombreCompleto
+
+//     let estado = prompt("Alumno: " + nombreCompletoAlumno + ".\n0 - Ausente. \n1 - Presente.")
+
+//     listadoAlumnos[i].agregarPresente(estado)
+
+// }
+
+// // Terminar calculando el porcentaje de ausentes de la clase y con un filter por alumnos ausentes
+// let ausentes = listadoAlumnos.filter(ausente => ausente.presentesAlumno[length] == 0).map(ausente => "\n" + ausente.nombreCompleto)
+// let porcentaAusentes = (ausentes.length / cantidadAlumnos) * 100;
+
+// alert("En la clase de hoy hubo " + porcentaAusentes + "% de ausentes.");
+// alert("Los alumnos ausentes fueron:" + ausentes + ".");
+
+// }
 
 
-////// APLICACION
+//////// APLICACION
 
 
-const usarApp = parseInt(Number(prompt(
-    "Elija cual opcion desea elegir:\n"+
-    "1 - Registrar un Alumno Nuevo.\n" +
-    "2 - Poner calificacion a los alumnos.\n"+
-    "3 - Tomar presentes de la clase."
-)));
+////// Botones de HTML
 
-switch (usarApp){
+//// Boton Registrar Alumno
+const button1 = document.querySelector("#registrarAlumno-btn");
 
-case (1):
-    // Registrar un Alumno Nuevo
-let registroNuevo = Number(prompt("¿Desea registrar un alumno nuevo? (1-Si) (2-No)"));
-if(registroNuevo === 1){
+button1.onclick = () => {
+
+  button1.style.cssText =
+    "background-color: green; color: white; border: none; padding: .5em; border-radius: 5px;";
+
+    document.getElementById("formularioRegistrarAlumno").style.display = "block";
     registrarAlumno();
-} else {
-    alert("No se registraron estudiantes nuevos/as");
+
 };
-    break
 
-case (2):
-    // Poner notas a los alumnos
+
+//// Boton Calificar Alumno
+const button2 = document.querySelector("#calificarAlumnos-btn");
+
+button2.onclick = () => {
+
+  button2.style.cssText =
+    "background-color: green; color: white; border: none; padding: .5em; border-radius: 5px;";
+
+    document.getElementById("formularioCalificarAlumnos").style.display = "block";
     calificarAlumnos();
-    break;
-
-case (3):
-    // Poner notas a los alumnos
-    tomarPresente();
-    break;
-
-default:
-    // Si el valor no es valido
-    alert("El valor indicado no es valido.")
-    break;
-
-}
+    
+};
